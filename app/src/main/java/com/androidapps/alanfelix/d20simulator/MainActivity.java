a008899a4d262944211f3805a9cb736767d1c93b
Alan Felix <alan@qrpoint.com.br>
2019-03-31 18:26:02 -0300

    Criação do menu com o botão de Minhas Jogadas.
    Signed-off-by: Alan Felix <alan@qrpoint.com.br>

--- app/src/main/java/com/androidapps/alanfelix/d20simulator/MainActivity.java
+++ app/src/main/java/com/androidapps/alanfelix/d20simulator/MainActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -54,11 +58,28 @@ public class MainActivity extends AppCompatActivity {
         alert.show();
     }
 
+    /* Adiciona o menu na toolbar */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        /* TOOLBAR */
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         /* ANIMACOES */
         final Animation diceshake = AnimationUtils.loadAnimation(this, R.anim.diceshake);
